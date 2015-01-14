cf7beae7f514737396b4dd54e3d6c9633acb5bba
Angus Griffith <16sn6uv@gmail.com>
2015-01-15 01:47:22 +1100

    add examples

---
+++ examples/comments.m
@@ -0,0 +1,16 @@
+(* 123 *)
+(* " fdsf *)
+(* fdsf " *)
+(* " fdsf " *)
+"(* sdfds "
+"sdfds *)"
+"(*pretend comment*)"
+(* """ *)
+(* (* *) *)
+
+(*
+
+    multiline comment
+    with nested (* *) and "" \
+
+end here *)
