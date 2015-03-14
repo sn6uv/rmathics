998c5f93477cc32ccdbf570ce107222568da527f
Angus Griffith <16sn6uv@gmail.com>
2015-03-14 13:36:08 +1100

    refactor kernel.py

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ PY = python2
 TR = /home/angus/prog/pypy-2.4.0-src/rpython/bin/rpython
 
 all: bin/
-	$(PY) $(TR) --output=bin/rmathics main.py
+	$(PY) $(TR) --output=bin/kernel kernel.py
 
 bin/:
 	mkdir bin/
--- kernel.py
+++ kernel.py
@@ -14,55 +14,71 @@ def read_contents(filename):
     return contents
 
 
-def lrstrip1(s):
-    '''
-    returns s[1:-1]
-    '''
-    istart = 1
-    istop = len(s) - 1
-    assert istop >= 1
-    return s[istart:istop]
+class Connection(object):
+    def __init__(self, contents):
+        contents = contents.strip()
+        if not (contents[0] == '{' and contents[-1] == '}'):
+            raise ValueError("Invalid enclosing {}")
+        for entry in self.lrstrip1(contents).split(','):
+            field, value = entry.split(':')
+            field = field.strip()
+            value = value.strip()
+            if not (field[0] == field[-1] == '"'):
+                raise ValueError("Invalid field %s" % field)
+            field = self.lrstrip1(field)
+            if value[0] == value[-1] == '"':
+                value = self.lrstrip1(value)
+                if field == 'transport':
+                    self.transport = value
+                elif field == 'signature_scheme':
+                    self.signature_scheme = value
+                elif field == 'ip':
+                    self.ip = value
+                elif field == 'key':
+                    self.key = value
+                else:
+                    raise ValueError("Unknown str field %s" % field)
+            elif value.isdigit():
+                value = int(value)
+                if field == 'control_port':
+                    self.control_port = value
+                elif field == 'shell_port':
+                    self.shell_port = value
+                elif field == 'stdin_port':
+                    self.stdin_port = value
+                elif field == 'hb_port':
+                    self.hb_port = value
+                elif field == 'iopub_port':
+                    self.iopub_port = value
+                else:
+                    raise ValueError("Unknown int field %s" % field)
+            else:
+                raise ValueError("Unknown value type %s" % value)
 
+    @staticmethod
+    def lrstrip1(s):
+        '''
+        returns s[1:-1]
+        '''
+        istart = 1
+        istop = len(s) - 1
+        assert istop >= 1
+        return s[istart:istop]
 
-def parse_json(contents):
-    '''
-    simple json parser
-    '''
-    result = {}
-    contents = contents.strip()
-    if not (contents[0] == '{' and contents[-1] == '}'):
-        print 1
-        raise ValueError("Invalid enclosing {}")
-    for entry in lrstrip1(contents).split(','):
-        field, value = entry.split(':')
-        field = field.strip()
-        value = value.strip()
-        if not (field[0] == field[-1] == '"'):
-            print 2
-            raise ValueError("Invalid field %s" % field)
-        field = lrstrip1(field)
-        if value[0] == value[-1] == '"':
-            # value = lrstrip1(value)
-            pass
-        elif value.isdigit():
-            # value = int(value)
-            pass
-        else:
-            print 3
-            raise ValueError("Invalid type %s" % value)
-        result[field] = value
-    return result
+    def debug(self):
+        print(self.control_port, self.shell_port, self.transport,
+              self.signature_scheme, self.stdin_port, self.hb_port, self.ip,
+              self.iopub_port, self.key)
 
 
 def entry_point(argv):
     if len(argv) != 2:
         return 1
     try:
-        connection = parse_json(read_contents(argv[1]))
+        connection = Connection(read_contents(argv[1]))
     except ValueError:
         return 1
-    for key in connection:
-        print(key, connection[key])
+    connection.debug()
     return 0
 
 
