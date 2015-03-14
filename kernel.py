f03f1dcc791bda94958c4b2263c29b12c9975944
Angus Griffith <16sn6uv@gmail.com>
2015-03-14 12:32:18 +1100

    begin rpython compliant ipython kernel

--- kernel.py
+++ kernel.py
@@ -1,58 +1,69 @@
-#! /usr/bin/env python2
-
+import os
 import sys
-from IPython.kernel.zmq.kernelbase import Kernel
-
-from rmathics.version import __version__ as version
-from rmathics.expression import Expression, Symbol, String
-from rmathics.definitions import Definitions
-from rmathics.parser import parse
-from rmathics.evaluation import evaluate
 
 
-class MathicsKernel(Kernel):
-    implementation = 'rmathics'
-    implementation_version = version
-    language = 'Wolfram'
-    language_version = '10.0'
-    language_info = {'mimetype': 'text/plain'}
-    banner = 'Mathics is a general-purpose computer algebra system'
-    definitions = Definitions()
+def read_contents(filename):
+    fp = os.open(filename, os.O_RDONLY, 0o777)
+    contents = ''
+    while True:
+        read = os.read(fp, 4096)
+        if len(read) == 0:
+            break
+        contents += read
+    os.close(fp)
+    return contents
 
-    def do_execute(self, code, silent, store_history=True,
-                   user_expressions=None, allow_stdin=False):
-        expr, messages = parse(code, self.definitions)
-        if not silent:
-            for message in messages:
-                stream_content = {
-                    'name': 'stderr',
-                    'text': self.definitions.construct_message(*message),
-                }
-                self.send_response(self.iopub_socket, 'stream', stream_content)
-        result, messages = evaluate(expr, self.definitions)
-        if not silent:
-            for message in messages:
-                stream_content = {
-                    'name': 'stderr',
-                    'text': self.definitions.construct_message(*message),
-                }
-                self.send_response(self.iopub_socket, 'stream', stream_content)
-            stream_content = {
-                'name': 'stdout',
-                'text': result.repr(),
-            }
-            self.send_response(self.iopub_socket, 'stream', stream_content)
-        return {
-            'status': 'ok',
-            'execution_count': self.execution_count,
-            'payload': [],
-            'user_expressions': {},
-        }
+
+def lrstrip1(s):
+    '''
+    returns s[1:-1]
+    '''
+    istart = 1
+    istop = len(s) - 1
+    assert istop >= 1
+    return s[istart:istop]
+
+
+def parse_json(contents):
+    '''
+    simple json parser
+    '''
+    result = {}
+    contents = contents.strip()
+    if not (contents[0] == '{' and contents[-1] == '}'):
+        print 1
+        raise ValueError("Invalid enclosing {}")
+    for entry in lrstrip1(contents).split(','):
+        field, value = entry.split(':')
+        field = field.strip()
+        value = value.strip()
+        if not (field[0] == field[-1] == '"'):
+            print 2
+            raise ValueError("Invalid field %s" % field)
+        field = lrstrip1(field)
+        if value[0] == value[-1] == '"':
+            # value = lrstrip1(value)
+            pass
+        elif value.isdigit():
+            # value = int(value)
+            pass
+        else:
+            print 3
+            raise ValueError("Invalid type %s" % value)
+        result[field] = value
+    return result
 
 
 def entry_point(argv):
-    from IPython.kernel.zmq.kernelapp import IPKernelApp
-    IPKernelApp.launch_instance(kernel_class=MathicsKernel)
+    if len(argv) != 2:
+        return 1
+    try:
+        connection = parse_json(read_contents(argv[1]))
+    except ValueError:
+        return 1
+    for key in connection:
+        print(key, connection[key])
+    return 0
 
 
 def target(*args):
